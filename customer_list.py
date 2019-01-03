35aa3318f2f30f4842455712297a451c1ed1d09b
vlad <vladshatalyuk@gmail.com>
2019-01-03 14:04:35 +0200

    Done

--- customer_list.py
+++ customer_list.py
@@ -8,28 +8,37 @@ list_of_customers = {
 
 
 def find_person(value):
-    if value in list_of_customers:
-        return list_of_customers.items()
+    x = list_of_customers.get(value)
+    if x:
+        print(x)
 
     else:
-        return False
+        print(value, "not in the list!")
 
 
-def person_register(key_for_register, value_for_register):
-    x = list_of_customers.setdefault(key_for_register, value_for_register)
-    print("", x)
-
-
-try:
-    if choice == 'yes':
-        key_to_find = input("Enter name to find: ")
-        find_person(key_to_find)
-    elif choice == 'no':
-        key_for_register = input("Enter your name: ")
-        value_for_register = input("Enter your email and phone number: ")
-        person_register(key_for_register, value_for_register)
-
+def person_register(key_register, *args,):
+    x = list_of_customers.setdefault(key_register, *args)
+    if x:
+        print("Register was success", x)
+        list_of_customers.update()
     else:
-        print("Unknown choice")
-except Exception as e:
-    print("Error: ", e)
+        print("Something gos wrong!")
+
+def of_choice(value):
+    try:
+        if value == 'yes':
+            key_to_find = input("Enter name to find: ")
+            find_person(key_to_find)
+        elif value == 'no':
+            key_for_register = input("Enter your name: ")
+            value_for_register = input("Enter your email and phone number: ")
+            person_register(key_for_register, value_for_register)
+
+        else:
+            print("Unknown choice")
+    except Exception as e:
+        print("Error: ", e)
+
+
+of_choice(choice)
+
